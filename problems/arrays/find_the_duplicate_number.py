5d68f474ade6e86c969c8d23706bb653d6cd4a29
Bemnet57 <bemnetasseged57@gmail.com>
2025-10-06 19:22:06 +0300

    Arrays: Find The Duplicated Number

---
+++ problems/arrays/find_the_duplicate_number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        uniqueNums = set()
+        for num in nums:
+            if num in uniqueNums:
+                return num
+            uniqueNums.add(num)
